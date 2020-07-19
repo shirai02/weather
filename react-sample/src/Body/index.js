90f8a0c5b90665a685a46a19c74009eaac615dde
shirai02 <kazuking.1911@gmail.com>
2020-07-19 22:29:12 +0900

    git init

---
+++ react-sample/src/Body/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styled/body.css';
+
+function Body() {
+    return (
+        <React.Fragment>
+            <div class="body">
+                <div class="cover">
+                    <div class="vertical-bar"></div>
+                    <div class="holizontal-bar"></div>
+                </div>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Body;
