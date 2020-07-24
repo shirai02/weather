58c3d507fa41b25628bf0635c0f5a5b8a73d86f8
shirai02 <kazuking.1911@gmail.com>
2020-07-24 19:41:33 +0900

    add borderline

--- react-sample/src/Body/index.js
+++ react-sample/src/Body/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import '../styled/body.scss';
 
+// TODO: API取得したい
+
 function Body() {
     return (
         <React.Fragment>
             <div class="body">
                 <div class="cover">
+
                     <h1>CSS Design Test</h1>
-                    <div class="bdT"></div>
+                    <div class="bdC"></div>
+                    {/* <div class="bdT"></div>
                     <div class="bdB"></div>
                     <div class="bdR"></div>
-                    <div class="bdL"></div>
+                    <div class="bdL"></div> */}
                 </div>
                 {/* <div class="left-vertical-bar"></div>
                 <div class="right-vertical-bar"></div>
