39843079452b5516028a95ede90774b65f035c03
shirai02 <kazuking.1911@gmail.com>
2020-07-24 14:33:36 +0900

    css animation test
    add border animation

--- react-sample/src/Body/index.js
+++ react-sample/src/Body/index.js
@@ -7,6 +7,10 @@ function Body() {
             <div class="body">
                 <div class="cover">
                     <h1>CSS Design Test</h1>
+                    <div class="bdT"></div>
+                    <div class="bdB"></div>
+                    <div class="bdR"></div>
+                    <div class="bdL"></div>
                 </div>
                 {/* <div class="left-vertical-bar"></div>
                 <div class="right-vertical-bar"></div>
